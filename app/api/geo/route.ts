8829f4a0fcce9adedfea94dbad4a246bf093fb0c
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2026-05-23 22:36:19 +0500

    new api fixed

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -1,10 +1,12 @@
 export const runtime = "edge";
 import { NextRequest, NextResponse } from "next/server";
+import { geolocation } from "@vercel/functions";
 
 export async function GET(req: NextRequest) {
+  const geo = geolocation(req);
   return NextResponse.json({
-    country: req.geo?.country || "Only available on production",
-    city: req.geo?.city || "Test on your deployed URL",
+    country: geo?.country || "Only available on production",
+    city: geo?.city || "Test on your deployed URL",
     runtime: "edge",
   });
 }
